3bd7374fce674ed6060491c41f2ccf5d40da6780
viola <violachandra2806@gmail.com>
2026-09-04 21:44:31 +0700

    To run it

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+
+  output: "export",
+  basePath: "/portofolio",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
